6b613484b5d81e476ae31cf7ba539e71f694824f
Villamor <113780247+Nott001@users.noreply.github.com>
2024-11-09 03:22:43 +0800

    updates

--- LAB_5/Activity5B_Class.py
+++ LAB_5/Activity5B_Class.py
@@ -4,16 +4,20 @@ student.collect_subject_data()
 
 def get_student_info(self):
     info = []
-    info.append("\t\t\t\t\tCERTIFICATE OF ENROLLMENT")
-    info.append(f"NAME  : {self.student_name:<20} STUDENT NO.: {self.student_number}")
-    info.append(f"COURSE: {self.student_course:<20} ACAD. YEAR : {self.student_year}")
+    info.append("\t\t\t\t\t\t\t\t\t\t\tCERTIFICATE OF ENROLLMENT")
+    info.append(f"NAME  : {self.student_name:<85} STUDENT NO.: {self.student_number}")
+    info.append(f"COURSE: {self.student_course:<85} ACAD. YEAR : {self.student_year}")
     info.append("-------------------------------------------------------------------")
     info.append("SECTION\t\t\t\tSUBJECT                                 UNITS")
     info.append("-------------------------------------------------------------------")
     for data in self.all_data:
         info.append(f"{data[1]:<15} {data[0]:<45} {data[2]}")
     info.append(f"                                             Total Units: {self.total_units}")
+    lines_needed = 43 - len(info)
+    info.extend([""] * lines_needed)
+    info.append("-----------------------------------------------------------------")
     info.append(f"                      Date Printed:{self.current_date}")
+    info.append("-----------------------------------------------------------------")
     return info
 
 def display_student_data(self):
