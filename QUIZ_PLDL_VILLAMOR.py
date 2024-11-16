eee42646abe446d280c440f79cfbe9753728443e
Villamor <113780247+Nott001@users.noreply.github.com>
2024-11-16 10:49:40 +0800

    exam

--- QUIZ_PLDL_VILLAMOR.py
+++ QUIZ_PLDL_VILLAMOR.py
@@ -47,81 +47,8 @@ class BillingInfo:
         self.total_current_amount = self.current_consumption + self.balance_prev_bill
         return self.total_current_amount
 
-    def display_all_data(self):
-        print("\n==================== Electric Bill ====================\n")
-        print(f"Summary for Customer Account Number (CAN) {self.customer_info.customer_account_num}\n")
-        print(f"Customer Name :{self.customer_info.customer_name}")
-        print(f"Address       :{self.customer_info.customer_street_add}, {self.customer_info.customer_barangay}, {self.customer_info.customer_city}\n")
 
-        print(f"|  Previous Balance  |  ", end="")
-        if self.balance_prev_bill == 0:
-            print("Thank you for your payment!")
-        else:
-            print(f"Please pay the amount of PHP {self.balance_prev_bill:.2f}")
 
-        print("\n|      Current Charges      |")
-        print(f"|   Amount due: PHP {self.total_current_amount:.2f}   |   Due Date: {self.due_date}   |")
-        print("|    Total amount Due    |")
-        print(f"|    PHP {self.total_current_amount:.2f}    |\n")
-
-        print("\n|----------------------------------------------|")
-        print("|                Service Info                  |")
-        print("|----------------------------------------------|")
-        print(f"|Service ID Number       : {self.customer_info.customer_account_num}")
-        print(f"|Rate                    : {self.customer_rate}")
-        print(f"|Contract In The Name of : {self.customer_info.customer_name}")
-        print(f"|Service Address         : {self.customer_info.customer_street_add}, {self.customer_info.customer_barangay}, {self.customer_info.customer_city}\n")
-
-        print("\n")
-        print("|----------------------------------------------|")
-        print("|                Billing Info                  |")
-        print("|----------------------------------------------|")
-        print(f"| Bill Date               : {self.bill_date}")
-        print(f"| Meter Reading Date      : {self.meter_reading_date}")
-        print(f"| Billing Period          : {self.billing_period}")
-        print(f"| Due Date                : {self.due_date}")
-        print(f"| Total kWh               : {self.actual_consumption}")
-        print(f"| Total Current Amount    : {self.total_current_amount:.2f}")
-        print(f"| Next Meter Reading      : {self.next_meter_reading}")
-        print("|----------------------------------------------|")
-
-        print("\n|----------------------------------------------|")
-        print("|                Your electric bill                 |")
-        print("|----------------------------------------------|")
-        print(f"|Bill Date               : {self.bill_date}")
-        print(f"|Bill Period             : {self.billing_period}")
-        print(f"|Meter Reading Date      : {self.meter_reading_date}")
-        print(f"|Next Meter Reading      : {self.next_meter_reading}")
-        print(f"|Customer Type           : {self.customer_info}")
-        print(f"|Due date                : {self.due_date}")
-        print(f"|Rate this month         : {self.rate_per_hour_res}")
-        print(f"|Total kWh               : {self.actual_consumption} kWh")
-        print("\n")
-
-        print("\n|----------------------------------------------|")
-        print("|        Breakdown of Electricity Charges      |")
-        print("|------------------------------------------------|")
-        print("|                  Please pay                 |")
-        print(f"Total Current Amount    : PHP {self.total_current_amount:.2f}\n")
-        print("|------------------------------------------------|")
-        print(f"Generation Fee          : PHP {self.generation:.2f}")
-        print(f"Transmission Fee        : PHP {self.transmission:.2f}")
-        print(f"System Loss Fee         : PHP {self.system_loss:.2f}")
-        print(f"Distribution Fee        : PHP {self.distribution:.2f}")
-        print(f"Subsidies Fee           : PHP {self.subsidies:.2f}")
-        print(f"Government Taxes        : PHP {self.government_taxes:.2f}")
-        print(f"Universal Charges       : PHP {self.universal_charges:.2f}")
-        print(f"Renewable Fee           : PHP {self.renewable:.2f}")
-        print(f"Applied Credits         : PHP {self.applied_credits:.2f}")
-        print(f"Other Charges           : PHP {self.other_charges:.2f}")
-        print(f"Installment Due         : PHP {self.installment_due:.2f}\n")
-        print(f"Total Current Amount    : PHP {self.total_current_amount:.2f}\n")
-        print("=======================================================\n")
-
-#calling of functions
-customer_info = CustomerInfo()
-billing_info = BillingInfo(customer_info)
-billing_info.display_all_data()
 
 
 
--- QUIZ_file_2.py
+++ QUIZ_file_2.py
@@ -1,53 +1,83 @@
-# main.py
+import QUIZ_PLDL_VILLAMOR
+customer = QUIZ_PLDL_VILLAMOR.CustomerInfo()
+billing = QUIZ_PLDL_VILLAMOR.BillingInfo(customer)
 
-from QUIZ_PLDL_VILLAMOR import CustomerInfo, BillingInfo
+def display_all_data():
+    customer_info = QUIZ_PLDL_VILLAMOR.CustomerInfo()
 
-def display_bill(billing_info):
-    total_amount_due = billing_info.calculate_total_current_amount()
     print("\n==================== Electric Bill ====================\n")
-    print(f"Summary for Customer Account Number (CAN) {billing_info.customer_info.customer_account_num}\n")
-    print(f"Customer Name :{billing_info.customer_info.customer_name}")
-    print(f"Address       :{billing_info.customer_info.customer_street_add}, {billing_info.customer_info.customer_barangay}, {billing_info.customer_info.customer_city}\n")
+    print(f"Summary for Customer Account Number (CAN) {customer_info.customer_account_num}\n")
+    print(f"Customer Name :{customer_info.customer_name}")
+    print(
+        f"Address       :{customer_info.customer_street_add}, {customer_info.customer_barangay}, {customer_info.customer_city}\n")
 
     print(f"|  Previous Balance  |  ", end="")
-    if billing_info.balance_prev_bill == 0:
+    if billing.balance_prev_bill == 0:
         print("Thank you for your payment!")
     else:
-        print(f"Please pay the amount of PHP {billing_info.balance_prev_bill:.2f}")
+        print(f"Please pay the amount of PHP {billing.balance_prev_bill:.2f}")
 
     print("\n|      Current Charges      |")
-    print(f"|   Amount due: PHP {total_amount_due:.2f}   |   Due Date: {billing_info.due_date}   |")
+    print(f"|   Amount due: PHP {billing.total_current_amount:.2f}   |   Due Date: {billing.due_date}   |")
     print("|    Total amount Due    |")
-    print(f"|    PHP {total_amount_due:.2f}    |\n")
+    print(f"|    PHP {billing.total_current_amount:.2f}    |\n")
 
     print("\n|----------------------------------------------|")
     print("|                Service Info                  |")
     print("|----------------------------------------------|")
-    print(f"|Service ID Number       : {billing_info.customer_info.customer_account_num}")
-    print(f"|Rate                    : {billing_info.customer_rate}")
-    print(f"|Contract In The Name of : {billing_info.customer_info.customer_name}")
-    print(f"|Service Address         : {billing_info.customer_info.customer_street_add}, {billing_info.customer_info.customer_barangay}, {billing_info.customer_info.customer_city}\n")
+    print(f"|Service ID Number       : {customer_info.customer_account_num}")
+    print(f"|Rate                    : {billing.customer_rate}")
+    print(f"|Contract In The Name of : {customer_info.customer_name}")
+    print(
+        f"|Service Address         : {customer_info.customer_street_add}, {customer_info.customer_barangay}, {customer_info.customer_city}\n")
 
-    print("\n|----------------------------------------------|")
+    print("\n")
+    print("|----------------------------------------------|")
     print("|                Billing Info                  |")
     print("|----------------------------------------------|")
-    print(f"| Bill Date               : {billing_info.bill_date}")
-    print(f"| Meter Reading Date      : {billing_info.meter_reading_date}")
-    print(f"| Billing Period          : {billing_info.billing_period}")
-    print(f"| Due Date                : {billing_info.due_date}")
-    print(f"| Total kWh               : {billing_info.actual_consumption}")
-    print(f"| Total Current Amount    : {total_amount_due:.2f}")
-    print(f"| Next Meter Reading      : {billing_info.next_meter_reading}")
+    print(f"| Bill Date               : {billing.bill_date}")
+    print(f"| Meter Reading Date      : {billing.meter_reading_date}")
+    print(f"| Billing Period          : {billing.billing_period}")
+    print(f"| Due Date                : {billing.due_date}")
+    print(f"| Total kWh               : {billing.actual_consumption}")
+    print(f"| Total Current Amount    : {billing.total_current_amount:.2f}")
+    print(f"| Next Meter Reading      : {billing.next_meter_reading}")
     print("|----------------------------------------------|")
 
-    print(f"\nTotal Amount Due: PHP {total_amount_due:.2f}")
-    print("=======================================================")
+    print("\n|----------------------------------------------|")
+    print("|                Your electric bill                 |")
+    print("|----------------------------------------------|")
+    print(f"|Bill Date               : {billing.bill_date}")
+    print(f"|Bill Period             : {billing.billing_period}")
+    print(f"|Meter Reading Date      : {billing.meter_reading_date}")
+    print(f"|Next Meter Reading      : {billing.next_meter_reading}")
+    print(f"|Customer Type           : {billing.customer_rate}")
+    print(f"|Due date                : {billing.due_date}")
+    print(f"|Rate this month         : {billing.rate_per_hour_res}")
+    print(f"|Total kWh               : {billing.actual_consumption} kWh")
+    print("\n")
 
-def main():
-    customer_info = CustomerInfo()
-    billing_info = BillingInfo(customer_info)
-    display_bill(billing_info)
+    print("\n|----------------------------------------------|")
+    print("|        Breakdown of Electricity Charges      |")
+    print("|------------------------------------------------|")
+    print("|                  Please pay                 |")
+    print(f"Total Current Amount    : PHP {billing.total_current_amount:.2f}\n")
+    print("|------------------------------------------------|")
+    print(f"Generation Fee          : PHP {billing.generation:.2f}")
+    print(f"Transmission Fee        : PHP {billing.transmission:.2f}")
+    print(f"System Loss Fee         : PHP {billing.system_loss:.2f}")
+    print(f"Distribution Fee        : PHP {billing.distribution:.2f}")
+    print(f"Subsidies Fee           : PHP {billing.subsidies:.2f}")
+    print(f"Government Taxes        : PHP {billing.government_taxes:.2f}")
+    print(f"Universal Charges       : PHP {billing.universal_charges:.2f}")
+    print(f"Renewable Fee           : PHP {billing.renewable:.2f}")
+    print(f"Applied Credits         : PHP {billing.applied_credits:.2f}")
+    print(f"Other Charges           : PHP {billing.other_charges:.2f}")
+    print(f"Installment Due         : PHP {billing.installment_due:.2f}\n")
+    print(f"Total Current Amount    : PHP {billing.total_current_amount:.2f}\n")
+    print("=======================================================\n")
+# Calling the function for display
+display_all_data()
 
-if __name__ == "__main__":
-    main()
+
 
